6e021f548c8574d132cd0df19c8118815219eaed
Lucia Brammer <brammerl96@gmail.com>
2020-07-14 23:22:50 -0700

    i give up

--- dailyRev.js
+++ dailyRev.js
@@ -1,12 +1,18 @@
 module.exports = (transactions) => {
   const dailyRevObj = {};
   transactions.forEach(transaction => {
-    const date = new Date(transaction.timestamp).toDateString();
-    if(!dailyRevObj[date]){
-      dailyRevObj[date] = transaction.price;
+    const date = new Date(transaction.timestamp);
+    const offset = date.getTimezoneOffset() * 60000;
+    console.log(offset);
+    const standardDate = date.setMilliseconds(date.getMilliseconds() - offset)
+
+    const newDate = new Date(standardDate).toDateString();
+    
+    if(!dailyRevObj[offset]){
+      dailyRevObj[newDate] = transaction.price;
     } 
 
-    dailyRevObj[date] += transaction.price;
+    dailyRevObj[newDate] += transaction.price;
   });
 
   return dailyRevObj;
